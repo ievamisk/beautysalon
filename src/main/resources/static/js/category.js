b80fb679245ec90eef616b32eb20d429a25fbc4b
ievamisk <ieva.misk@gmail.com>
2017-12-20 22:01:37 +0200

    Categories & subcategories management (add, edit, delete)

---
+++ src/main/resources/static/js/category.js
@@ -0,0 +1,105 @@
+app.service('categoryService', function ($http) {
+    this.categories = [];
+
+    this.getListRequest = function () {
+        var self = this;
+        var request = {
+            url: '/category/',
+            method: 'GET'
+        };
+        $http(request).then(function (response) {
+            self.categories = response.data;
+        }, function(error) {
+            console.log('error', error);
+        })
+    };
+
+    this.addNewCategory = function (category, callback) {
+        var request = {
+            url: '/category/add',
+            method: 'POST',
+            params: {
+                category: category
+            }
+        };
+        $http(request).then(function (response) {
+            callback(response);
+        }, function(error) {
+            callback(error);
+        })
+    };
+
+    this.deleteCategory = function (category, callback) {
+        console.log(category);
+        var request = {
+            url: '/category/'+ category,
+            method: 'DELETE'
+        };
+        $http(request).then(function (response) {
+            callback(response);
+        }, function(error) {
+            callback(error);
+        })
+    };
+
+    this.editCategory = function (id, category, callback) {
+        var request = {
+            url: '/category/' + id,
+            method: 'PUT',
+            params: {
+                category: category
+            }
+        };
+        $http(request).then(function (response) {
+            callback(response);
+        }, function(error) {
+            callback(error);
+        })
+    };
+});
+
+app.controller('categoryController', function ($scope, categoryService) {
+    $scope.categories = [];
+    $scope.service = categoryService;
+
+    $scope.editCategoryId = '';
+    $scope.editCategoryName = '';
+
+    $scope.$watch('service.categories',function (newVal) {
+        $scope.categories = newVal;
+    });
+
+    $scope.onInit = function () {
+        categoryService.getListRequest();
+    };
+
+    $scope.addNewCategory = function () {
+        if($scope.category !== ""){
+            categoryService.addNewCategory($scope.category, function (response) {
+                console.log('from category controller', response);
+                $scope.service.getListRequest();
+            });
+        }
+    };
+
+    $scope.deleteCategory = function (event) {
+        var id = event.target.getAttribute('data-category');
+        console.log(event.target);
+        categoryService.deleteCategory(id, function (response) {
+            console.log("deleted", response);
+            categoryService.getListRequest();
+        });
+    };
+
+    $scope.setupEditCategory = function (id) {
+        $scope.editCategoryId = id;
+    };
+
+    $scope.editCategory = function () {
+        categoryService.editCategory($scope.editCategoryId, $scope.editCategoryName, function (response) {
+            console.log("updated", response);
+            categoryService.getListRequest();
+        })
+
+    }
+});
---
+++ src/main/resources/static/js/subcategory.js
@@ -0,0 +1,106 @@
+app.service('subcategoryService', function ($http) {
+    this.subcategories = [];
+    this.categoryId = '';
+    this.subcategoryId = '';
+
+    this.addNewSubcategory = function (subcategory, categoryId, callback) {
+        var request = {
+            url: '/subcategory/add',
+            method: 'POST',
+            params: {
+                subcategory: subcategory,
+                category_id: categoryId
+            }
+        };
+        $http(request).then(function (response) {
+            callback(response);
+        }, function(error) {
+            callback(error);
+        })
+    };
+
+    this.deleteSubcategory = function (subcategory, callback) {
+        console.log(subcategory);
+        var request = {
+            url: '/subcategory/'+ subcategory,
+            method: 'DELETE'
+        };
+        $http(request).then(function (response) {
+            callback(response);
+        }, function(error) {
+            callback(error);
+        })
+    };
+
+    this.editSubcategory = function (subcategoryId, subcategoryName, callback) {
+        var request = {
+            url: '/subcategory/'+ subcategoryId,
+            method: 'PUT',
+            params: {
+                subcategory: subcategoryName
+            }
+
+        };
+        $http(request).then(function (response) {
+            callback(response);
+        }, function(error) {
+            callback(error);
+        })
+    }
+});
+
+app.controller('subcategoryController', function ($scope, subcategoryService, categoryService) {
+    $scope.subcategories = [];
+    $scope.service = subcategoryService;
+    $scope.categoryId = '';
+    $scope.subcategoryId = '';
+    $scope.subcategoryName = '';
+
+    $scope.setupSubcategory = function (categoryId) {
+        subcategoryService.categoryId = categoryId;
+        $scope.categoryId = categoryId;
+    };
+
+    $scope.setupEditSubcategory = function (subcategoryId) {
+        subcategoryService.subcategoryId = subcategoryId;
+        $scope.subcategoryId = subcategoryId;
+    };
+
+    $scope.$watch('service.subcategories',function (newVal) {
+        $scope.subcategories = newVal;
+    });
+
+    $scope.$watch('service.categoryId', function (newVal) {
+        $scope.categoryId = newVal;
+    });
+
+    $scope.$watch('service.subcategoryId', function (newVal) {
+        $scope.subcategoryId = newVal;
+    });
+
+    $scope.addNewSubcategory = function () {
+        console.log($scope.categoryId, $scope.subcategoryName);
+
+        subcategoryService.addNewSubcategory($scope.subcategoryName, $scope.categoryId, function (response) {
+            console.log('from subcategory controller', response);
+            categoryService.getListRequest();
+        });
+    };
+
+    $scope.deleteSubcategory = function (event) {
+        var id = event.target.getAttribute('data-subcategory');
+        subcategoryService.deleteSubcategory(id, function (response) {
+            console.log("deleted", response);
+            categoryService.getListRequest();
+        });
+    };
+
+    $scope.editSubcategory = function () {
+        console.log($scope.subcategoryId, $scope.subcategoryName);
+        subcategoryService.editSubcategory($scope.subcategoryId, $scope.subcategoryName, function (response) {
+            console.log($scope.subcategoryName, $scope.subcategoryId);
+            console.log("updated", response);
+            categoryService.getListRequest();
+        })
+    }
+});
